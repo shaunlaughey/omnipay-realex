28c150470d00fd9c5cbd3d4f4e538f31e43cdb25
Shaun Laughey <shaun@laughey.com>
2015-11-30 17:03:25 +0000

    Sha1 hash fix

--- src/Omnipay/Realex/Message/UpdateCardRequest.php
+++ src/Omnipay/Realex/Message/UpdateCardRequest.php
@@ -6,98 +6,90 @@
 
 namespace Omnipay\Realex\Message;
 
+class UpdateCardRequest extends RemoteAbstractRequest {
+	protected $endpoint = 'https://epage.payandshop.com/epage-remote-plugins.cgi';
 
-class UpdateCardRequest extends RemoteAbstractRequest
-{
-    protected $endpoint = 'https://epage.payandshop.com/epage-remote-plugins.cgi';
+	public function getCustomerRef() {
+		return $this->getParameter('customerRef');
+	}
 
-    public function getCustomerRef()
-    {
-        return $this->getParameter('customerRef');
-    }
+	public function setCustomerRef($customerRef) {
+		$this->setParameter('customerRef', $customerRef);
+	}
 
-    public function setCustomerRef($customerRef)
-    {
-        $this->setParameter('customerRef', $customerRef);
-    }
+	public function getData() {
+		// Create the hash
+		$timestamp = strftime("%Y%m%d%H%M%S");
+		$merchantId = $this->getMerchantId();
+		$orderId = $this->getTransactionId();
+		$secret = $this->getSecret();
+		$payerRef = $this->getCustomerRef();
 
-    public function getData()
-    {
-        // Create the hash
-        $timestamp  = strftime("%Y%m%d%H%M%S");
-        $merchantId = $this->getMerchantId();
-        $orderId    = $this->getTransactionId();
-        $secret     = $this->getSecret();
-        $payerRef   = $this->getCustomerRef();
+		/**
+		 * @var \Omnipay\Common\CreditCard $card
+		 */
+		$card = $this->getCard();
+		// Timestamp.merchantID.payerref.ref.expirydate.cardnumber
+		$tmp = "$timestamp.$merchantId.$payerRef.{$this->getCardReference()}.{$card->getExpiryDate('my')}.{$card->getNumber()}";
+		error_log($tmp);
+		$sha1hash = sha1($tmp);
+		$tmp2 = "$sha1hash.$secret";
+		$sha1hash = sha1($tmp2);
 
-        /**
-         * @var \Omnipay\Common\CreditCard $card
-         */
-        $card = $this->getCard();
-        // Timestamp.merchantID.payerref.ref.expirydate.cardnumber 
-        //$tmp = "$timestamp.$merchantId.$orderId.$amount.$currency.$payerRef";
-        // $tmp      = "$timestamp.$merchantId.$orderId...$payerRef.{$card->getBillingName()}.{$card->getNumber()}";
-        $tmp = "$timestamp.$merchantId.$payerRef.{$this->getCardReference()}. $card->getExpiryDate('my').{$card->getNumber()}";
-        $sha1hash = sha1($tmp);
-        $tmp2     = "$sha1hash.$secret";
-        $sha1hash = sha1($tmp2);
+		$domTree = new \DOMDocument('1.0', 'UTF-8');
 
-        $domTree = new \DOMDocument('1.0', 'UTF-8');
+		// root element
+		$root = $domTree->createElement('request');
+		$root->setAttribute('type', 'card-update-card');
+		$root->setAttribute('timestamp', $timestamp);
+		$root = $domTree->appendChild($root);
 
-        // root element
-        $root = $domTree->createElement('request');
-        $root->setAttribute('type', 'card-update-card');
-        $root->setAttribute('timestamp', $timestamp);
-        $root = $domTree->appendChild($root);
+		// merchant ID
+		$merchantEl = $domTree->createElement('merchantid', $merchantId);
+		$root->appendChild($merchantEl);
 
-        // merchant ID
-        $merchantEl = $domTree->createElement('merchantid', $merchantId);
-        $root->appendChild($merchantEl);
+		// order ID
+		$merchantEl = $domTree->createElement('orderid', $orderId);
+		$root->appendChild($merchantEl);
 
-        // order ID
-        $merchantEl = $domTree->createElement('orderid', $orderId);
-        $root->appendChild($merchantEl);
+		$cardEl = $domTree->createElement('card');
 
-        $cardEl = $domTree->createElement('card');
+		$cardRefEl = $domTree->createElement('ref', $this->getCardReference());
+		$cardEl->appendChild($cardRefEl);
 
-        $cardRefEl = $domTree->createElement('ref', $this->getCardReference());
-        $cardEl->appendChild($cardRefEl);
+		$payerRefEl = $domTree->createElement('payerref', $this->getCustomerRef());
+		$cardEl->appendChild($payerRefEl);
 
-        $payerRefEl = $domTree->createElement('payerref', $this->getCustomerRef());
-        $cardEl->appendChild($payerRefEl);
+		$numberEl = $domTree->createElement('number', $card->getNumber());
+		$cardEl->appendChild($numberEl);
 
-        $numberEl = $domTree->createElement('number', $card->getNumber());
-        $cardEl->appendChild($numberEl);
+		$expDateEl = $domTree->createElement('expdate', $card->getExpiryDate("my"));
+		$cardEl->appendChild($expDateEl);
 
-        $expDateEl = $domTree->createElement('expdate', $card->getExpiryDate("my"));
-        $cardEl->appendChild($expDateEl);
+		$chNameEl = $domTree->createElement('chname', $card->getBillingName());
+		$cardEl->appendChild($chNameEl);
 
-        $chNameEl = $domTree->createElement('chname', $card->getBillingName());
-        $cardEl->appendChild($chNameEl);
+		$typeEl = $domTree->createElement('type', $this->getCardBrand());
+		$cardEl->appendChild($typeEl);
 
-        $typeEl = $domTree->createElement('type', $this->getCardBrand());
-        $cardEl->appendChild($typeEl);
+		$issueNoEl = $domTree->createElement('issueno', $card->getIssueNumber());
+		$cardEl->appendChild($issueNoEl);
 
-        $issueNoEl = $domTree->createElement('issueno', $card->getIssueNumber());
-        $cardEl->appendChild($issueNoEl);
+		$root->appendChild($cardEl);
 
-        $root->appendChild($cardEl);
+		$sha1El = $domTree->createElement('sha1hash', $sha1hash);
+		$root->appendChild($sha1El);
 
-        $sha1El = $domTree->createElement('sha1hash', $sha1hash);
-        $root->appendChild($sha1El);
+		$xmlString = $domTree->saveXML($root);
 
-        $xmlString = $domTree->saveXML($root);
+		return $xmlString;
+	}
 
-        return $xmlString;
-    }
+	protected function createResponse($data) {
+		return $this->response = new UpdateCardResponse($this, $data);
+	}
 
-    protected function createResponse($data)
-    {
-        return $this->response = new UpdateCardResponse($this, $data);
-    }
-
-    public function getEndpoint()
-    {
-        return $this->endpoint;
-    }
+	public function getEndpoint() {
+		return $this->endpoint;
+	}
 }
